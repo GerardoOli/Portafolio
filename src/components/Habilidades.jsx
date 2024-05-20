b0cb48db530748221b61702294b18899832fbbb1
Senpai1998 <gera73.372@gmail.com>
2024-05-20 00:56:41 -0600

    first commit

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -5,12 +5,12 @@ const Habilidades = () => {
       <div className="flex flex-row justify-center mt-4 flex-wrap gap-8 sm:flex-col sm:items-center sm:gap-8">
         {/* Aquí puedes agregar contenido adicional si es necesario */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12 place-items-center mt-16 mb-16">
-          <img src="./public/image/html.png" alt="Habilidad 1" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
-          <img src="./public/image/css.png" alt="Habilidad 2" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
-          <img src="./public/image/Js.png" alt="Habilidad 3" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
-          <img src="./public/image/tailwinCss.png" alt="Habilidad 4" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
-          <img src="./public/image/bost.png" alt="Habilidad 5" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
-          <img src="./public/image/github.png" alt="Habilidad 6" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
+          <img src="/image/html.png" alt="Habilidad 1" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
+          <img src="/image/css.png" alt="Habilidad 2" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
+          <img src="/image/Js.png" alt="Habilidad 3" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
+          <img src="/image/tailwinCss.png" alt="Habilidad 4" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
+          <img src="/image/bost.png" alt="Habilidad 5" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
+          <img src="/image/github.png" alt="Habilidad 6" className="w-24 h-24 sm:w-full sm:h-auto md:w-32 md:h-32 rounded-full shadow-lg transform transition duration-300 hover:scale-110"/>
         </div>
 
       </div>
